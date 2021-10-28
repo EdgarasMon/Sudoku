77acd6486e02b65993a54510d50d7ecae5371b51
EdgarasMon <edgarasxxx@gmail.com>
2021-10-28 11:54:03 +0300

    Working algorithm for one row

--- s2.js
+++ s2.js
@@ -1,37 +1,23 @@
+let sudoku = [];
+var random;
 
-var sudoku = [];
+for (let x = 1; x < 10; x++) {
+    random = Math.floor(Math.random() * (9 - 1 + 1) + 1);
+    console.log("random", random);
 
-for (var x=1; x < 10; x++) {
-   // sudoku.push(x);
-   // console.log(sudoku);
-   //var previousEl = sudoku[x-1];
-   //var currentEl = sudoku[x];
-   random1 = Math.floor(Math.random()*10);
-   console.log("random1", random1);
-    if (random1 in sudoku) {
-        console.log("Yra toks elementas")
-        random2 = Math.floor(Math.random()*10);
-        console.log("random2", random2);
-        if (random2 in sudoku){
-            random3 = Math.floor(Math.random()*10);
-            console.log("random3", random3);
-            console.log("nera tokio elemento irasom random3")
-            sudoku.push(random3);
-        }else{
-        console.log("nera tokio elemento irasom random2")    
-        sudoku.push(random2);
+    if ( sudoku.includes(random) ) {
+        console.log("Yra toks elementas");
+
+        while ( sudoku.includes(random) ) {
+            console.log(random, "YRA");
+            random = Math.floor(Math.random() * (9 - 1 + 1) + 1);
         }
-    }else{
-        console.log("nera tokio elemento irasom random1")
-        sudoku.push(random1);
-
+        console.log("RANDOM REIKSME PASIBAIGUS FOR ", random);
+        sudoku.push(parseInt(random));
+    } else {
+        console.log("nera tokio elemento irasom random");
+        sudoku.push(random);
     }
- // Math.floor(Math.random()*10)
-    console.log("elem: ", sudoku);
+    console.log("sudoku: ", sudoku);
  }
 
-
-
- /*
- Jei toks elementas yra sarase, daryti random kol sugeneruos nesanti sarase elementa
- */
--- sudokuAlgorithm.js
+++ sudokuAlgorithm.js
@@ -3,49 +3,21 @@ var sudoku = [];
 function createSudoku() {
 for (var x=1; x < 10; x++) {
   for (var y=1; y < 10; y++) {
-    //var sudokuTemp = [null];
     coordinateX = parseInt(x);
     //console.log(coordinateX);
     coordinateY = parseInt(y);
     //console.log(coordinateY);
     //sudoku += "x" + coordinateX;
     //sudoku += "y" + coordinateY;
-    //sudoku += Math.floor(Math.random()*10); // generuoja random visiem elementams
     sudoku.push(y);
-     if (sudoku[x-1] == sudoku[x] in sudoku) {
-      sudoku.push(y);
-      // Math.floor(Math.random()*10)
-      console.log(sudoku);
-    }
+
   }
-  //Array.from(sudoku);
-  //console.log(Array.from(sudoku)); // spausdina kaip masyva bet yra stringas
-  //console.log(sudoku);
  }
  return sudoku;
 }
 
 
-
-function shuffle(array) {
-  array.sort(() => Math.random() - 0.5);
-}
-
-let sudokuArr = createSudoku();
-//Array.from(sudokuArr);
-//[...sudokuArr];
-//Object.assign([], sudokuArr);
-//shuffle(sudokuArr);
-
-//console.log(sudokuArr);
 console.log(createSudoku());
-
-//console.log(Array.prototype.slice(createSudoku()[0][0]));
-
-
-// gali buti kad vykdant konkatinacijai sudetus elementus nesupranta kaip atskiru 
-// elementu, uz funkcijos createSudoku ribu neiseina manipuliuoti elementais nors 
-// ir prisiskitiant nauja kintamaji kaip masyva..
 
 
 
